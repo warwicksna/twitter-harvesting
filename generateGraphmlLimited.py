824f82540b2fbf6616248895011c9fb4c94d2a38
k <git@thursday.eml.cc>
2012-03-05 16:52:24 +0000

    Add tweet-edges

--- generateGraphmlLimited.py
+++ generateGraphmlLimited.py
@@ -8,10 +8,11 @@ dupnodes = set([])
 nodes = ""
 edges = ""
 edgeID = ""
+print "<graphml edgedefault=\"directed\">\n<key id=\"name\" for=\"edge\" attr.name=\"name\" attr.type=\"string\"/>\n<graph edgedefault=\"directed\">"
 for user in base:
     uid = user[0];
     name = json.loads(user[3])[0]["screen_name"]
-    nodes += "<node id=\""+uid+"\"><data key=\"name\">"+name+"</data></node>\n"
+    print "<node id=\""+uid+"\"><data key=\"name\">"+name+"</data></node>"
     dupnodes.add(str(uid))
 #print dupnodes
 base = curse.execute("select uid, following, followers, targetinfo from gotcha")
@@ -22,13 +23,24 @@ for user in base:
     for toid in following:
         edgeID = str(uid)+"."+str(toid)
         if(edgeID not in dupedges and str(toid) in dupnodes):
-            edges += "<edge  source=\""+str(uid)+"\" target=\""+str(toid)+"\"/>\n" #id=\"e"+edgeID+"\"
+            print "<edge  source=\""+str(uid)+"\" target=\""+str(toid)+"\"/>" #id=\"e"+edgeID+"\"
             dupedges.add(edgeID)
     for fromid in followers:
         edgeID = str(fromid)+"."+str(uid)
         if(edgeID not in dupedges and str(fromid) in dupnodes):
-            edges += "<edge  source=\""+str(fromid)+"\" target=\""+str(uid)+"\"/>\n" #id=\"e"+edgeID+"\"
+            print "<edge  source=\""+str(fromid)+"\" target=\""+str(uid)+"\"/>" #id=\"e"+edgeID+"\"
             dupedges.add(edgeID)
         
-graphml = "<graphml edgedefault=\"directed\">\n  <key id=\"name\" for=\"edge\" attr.name=\"name\" attr.type=\"string\"/> <graph edgedefault=\"directed\">\n" + nodes + edges + "</graph>\n</graphml>";
-print graphml
+base = curse.execute("select uid, tweets, followers, following from gotcha")
+for user in base:
+    uid = json.loads(user[0])
+    for tweet in json.loads(user[1]):
+        if("retweeted_status" in tweet):
+            toid = tweet["retweeted_status"]["user"]["id"]
+        else:
+            toid = tweet["in_reply_to_user_id"]
+        if(toid):
+            if(toid not in json.loads(user[2]) and toid not in json.loads(user[3]) and str(toid) in dupnodes and str(fromid) in dupnodes): #assumes direction is irrelevant; (check user[2] only otherwise)
+                print "<edge source=\""+str(uid)+"\" target=\""+str(toid)+"\"/>"
+
+print "</graph>\n</graphml>";
